47d2b99e726170ac77e5740c852023badaf6e01f
Tiago Cordeiro <tiago@mulhergorila.com>
2020-05-29 13:58:47 -0300

    Resolvendo algumas Issues.
    Issue #7
    - Melhora documentação
    
    Issue #8
    - Melhora cobertura de testes
    
    - Issue #11
    Configura travis CI
    
    This close #7, close #8 and close #11

--- certgen/urls.py
+++ certgen/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from certgen import settings
 
 admin.site.site_header = 'Instituto Cultiva'
 
@@ -23,4 +26,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('core.urls')),
     path('', include('certificado.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- certificado/tests.py
+++ certificado/tests.py
@@ -1,3 +1,92 @@
-from django.test import TestCase
+from django.contrib.auth.models import User, Group
+from django.test import RequestFactory, Client, TransactionTestCase
+from django.urls import reverse
 
-# Create your tests here.
+from certificado.models import Certificado, make_slug
+
+
+class CertificadoTestCase(TransactionTestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.client = Client()
+
+        # Staff user
+        self.user_staff = User.objects.create_user(username='jacob',
+                                                   email='jacob@…',
+                                                   password='top_secret')
+        self.group = Group.objects.create(name='Testes')
+        self.group.user_set.add(self.user_staff)
+
+        self.certificado = Certificado.objects.create(aluno='Aluno Teste',
+                                                      curso='Curso Teste',
+                                                      carga_horaria='2',
+                                                      data='2020-04-23',
+                                                      parceria=1,
+                                                      modalidade=1,
+                                                      slug=make_slug())
+
+    def teste_view_certificados_logado(self):
+        self.client.force_login(self.user_staff)
+        request = self.client.get(reverse('lista_certificados'))
+
+        self.assertEqual(request.status_code, 200)
+
+    def teste_view_certificados_anonimo(self):
+        self.client.logout()
+        request = self.client.get(reverse('lista_certificados'))
+
+        self.assertEqual(request.status_code, 302)
+        self.assertRedirects(request,
+                             '/accounts/login/?next=/certificados/',
+                             status_code=302,
+                             target_status_code=200)
+
+    def teste_view_certificado_download_status_code(self):
+        self.client.logout()
+        request = self.client.get(reverse('download_certificado',
+                                          kwargs={
+                                              'pk': self.certificado.pk,
+                                              'slug': self.certificado.slug}))
+
+        self.assertEqual(request.status_code, 200)
+
+    def teste_view_certificado_novo_logado(self):
+        self.client.force_login(self.user_staff)
+        request = self.client.get(reverse('novo_certificado'))
+
+        self.assertEqual(request.status_code, 200)
+
+    def teste_view_certificado_novo_anonimo(self):
+        self.client.logout()
+        request = self.client.get(reverse('novo_certificado'))
+
+        self.assertEqual(request.status_code, 302)
+        self.assertRedirects(request,
+                             '/accounts/login/?next=/certificado/novo/',
+                             status_code=302,
+                             target_status_code=200)
+
+    def teste_novo_certificado_cadastro(self):
+        certificados = Certificado.objects.all()
+        self.assertEqual(len(certificados), 1)
+        self.assertEqual(Certificado.objects.count(), 1)
+
+        novo_certificado = {
+            'aluno': 'Novo Aluno Teste',
+            'curso': 'Curso Teste',
+            'carga_horaria': '2',
+            'data': '2020-04-23',
+            'parceria': '1',
+            'modalidade': '1',
+        }
+
+        self.client.force_login(self.user_staff)
+
+        response = self.client.post(reverse('novo_certificado'),
+                                    data=novo_certificado)
+        self.assertEqual(response.status_code, 302)
+
+        certificados = Certificado.objects.all()
+        self.assertEqual(len(certificados), 2)
+        self.assertEqual(Certificado.objects.count(), 2)
